7f60e7cc27736e22cfd2d88e489ab5fc0f42e5b8
chiragchandnani <chiragchandnani@gmail.com>
2019-04-02 13:12:56 +0530

    added login interceptor.

---
+++ BTP Convention/src/com/BTP/actions/supervisor/SelectReviewerAction.java
@@ -0,0 +1,12 @@
+package com.BTP.actions.supervisor;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class SelectReviewerAction extends ActionSupport{
+	
+	public String execute() {
+		System.out.println("in selecct reviewer action");
+		return SUCCESS;
+	}
+
+}
